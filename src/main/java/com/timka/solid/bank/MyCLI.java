05ffd030365a02a9bd4908539cb8703a2ae1e668
Temirbolat Amanzhol <amanzhol.temirbolat@jits-3-28.jits.ipa.dc>
2022-05-03 15:37:29 +0600

    Initial commit

---
+++ src/main/java/com/timka/solid/bank/MyCLI.java
@@ -0,0 +1,20 @@
+package com.timka.solid.bank;
+
+import java.util.Scanner;
+
+public class MyCLI {
+    private Scanner scanner;
+
+    public MyCLI(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public double requestClientAmount() {
+        return 0.0;
+    }
+    public String requestClientAccountNumber() {
+        return "";
+    }
+    public AccountType
+
+}
